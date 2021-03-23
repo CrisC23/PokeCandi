5275cbc4162db6eb3f053d9720d800dd14e67458
William <wperez185@yahoo.com>
2021-03-23 19:09:48 -0400

    api data

--- js/main.js
+++ js/main.js
@@ -5,19 +5,37 @@
 //   });
 // https://pokeapi.co/api/v2/pokemon/
 
-  $.ajax("https://pokeapi.co/api/v2/pokedex/26/", {
+$.ajax("https://pokeapi.co/api/v2/pokedex/26/", {
   // $.ajax("https://pokeapi.co/api/v2/region/1", {
-    success: function(data) {
-      // NEED TO LOOP TO GET INDIVIDUAL POKEMON
-    $.ajax(`https://pokeapi.co/api/v2/pokemon/${data.pokemon_entries[0].pokemon_species.name}`, {
-    success: function(pokemon) {
-     console.log(pokemon)
-     $("#poke-data").html(`<h4>${data.pokemon_entries[0].pokemon_species.name.toUpperCase()}</h4><img src=${pokemon.sprites.front_default}>`)
-},
-    error: function() {
-    }
-  });
-},
-    error: function() {
-    }
-  });
+  success: function (data) {
+    // NEED TO LOOP TO GET INDIVIDUAL POKEMON
+      
+      data.pokemon_entries.forEach((p) => {
+        $.ajax(
+          `https://pokeapi.co/api/v2/pokemon/${p.pokemon_species.name}`,
+          {
+            success: function (pokemon) {
+              // console.log(pokemon.id)
+              // console.log("ppppp ", pokemon);
+              $("#poke-data").append(
+                `<div class="test" data-poke-id="${pokemon.id}"><h4>${p.pokemon_species.name.toUpperCase()}</h4> <h4 class="poke-id">${pokemon.id}</h4><img src=${
+                  pokemon.sprites.front_default
+                }></div>`
+                );
+              },
+              error: function () {},
+            }
+            );
+          } )
+  },
+  error: function () {},
+});
+
+var divList = $(".test");
+console.log(divList)
+divList.sort(function(a, b){
+  console.log('aaaaa ', a)
+    return $(a).data("poke-id")-$(b).data("poke-id")
+});
+$("#poke-data").html(divList);
+
